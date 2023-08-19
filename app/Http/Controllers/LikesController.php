0fb67dcaa4e2eee183f656934dfbcb38aea15956
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-19 13:37:38 +0300

    Server <laravel> created likes apis

---
+++ app/Http/Controllers/LikesController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use Exception;
+use Illuminate\Http\Request;
+use App\Models\Recipe;
+use App\Models\Like;
+use Illuminate\Support\Facades\Auth;
+use PDO;
+
+class LikesController extends Controller
+{
+    public function store(Request $request_info){
+        try{
+            $user = Auth::user();
+            $validated_data = $this->validate($request_info, [
+                'recipe_id' => ['required', 'numeric'],
+            ]); 
+
+            $recipe = Like::create([
+                'recipe_id' => $validated_data['recipe_id'],
+                'user_id' => $user->id,
+            ]);
+
+            return $this->customResponse($recipe, 'recipe Created Successfully');
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    public function destroy($id){
+        try{
+            $like = Like::find($id);
+            $like->delete();
+            return $this->customResponse($like, 'Deleted Successfully');
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    public function countLikes($id){
+        try {
+            $recipeLikes =Like::where('recipe_id', $id)->count();
+
+            return $this->customResponse($recipeLikes);
+        } catch(Exception $e) {
+            return $this->customResponse($e->getMessage(), 'error', 500);
+        }
+    }
+
+    public function isLiked($id){
+        try{
+            $user = Auth::user();
+            $liked = Like::where('user_id', $user->id)
+                     ->where('recipe_id', $id)
+                     ->exists();
+
+            return $this->customResponse(['is_liked' => $liked], 'Success');
+        } catch(Exception $e) {
+            return $this->customResponse($e->getMessage(), 'error', 500);
+        }
+    }
+
+    function customResponse($data, $status = 'success', $code = 200){
+        $response = ['status' => $status,'data' => $data];
+        return response()->json($response,$code);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RecipesController;
+use App\Http\Controllers\LikesController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -19,6 +20,13 @@ Route::group(['middleware' => ['jwt.auth']], function () {
         Route::get('/{recipe}', [RecipesController::class, "getById"]);
         Route::post('/store', [RecipesController::class, "store"]);
         Route::delete('/destroy/{id}', [RecipesController::class, "destroy"]);
+    });
+
+    Route::group(['prefix' => 'like'], function(){
+        Route::get('/{id}', [LikesController::class, "isLiked"]);
+        Route::get('/count/{id}', [LikesController::class, "countLikes"]);
+        Route::post('/store', [LikesController::class, "store"]);
+        Route::delete('/destroy/{id}', [LikesController::class, "destroy"]);
     });
 
     Route::post('logout', [AuthController::class, 'logout']);
