2e5baf4c2aa8000fcccc7d31b827731fb809e181
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-19 13:07:06 +0300

    Server <laravel> created recipes apis

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use App\Models\User;
+use App\Models\Recipe;
+
+class RecipesController extends Controller
+{
+    public function store(Request $request_info){
+        try{
+            $user = Auth::user();
+            $validated_data = $this->validate($request_info, [
+                'name' => ['required', 'string'],
+                'cuisine' => ['string'],
+                'ingredients' => ['string'],
+                'image' => ['required', 'string'],
+            ]); 
+
+            $base64Image = $request_info->image;
+            echo $base64Image;
+
+            $decodedImage = base64_decode($base64Image);
+            $filename = 'recipe_' . time() . '.jpg';
+            $path = Storage::disk('public')->put('recipe_images/' . $filename, $decodedImage);
+
+            $recipe = Recipe::create([
+                'name' => $validated_data['name'],
+                'cuisine' => $validated_data['cuisine'],
+                'ingredients' => $validated_data['ingredients'],
+                'image_url' => 'recipe_images/' . $filename,
+                'user_id' => $user->id,
+            ]);
+
+            return $this->customResponse($recipe, 'recipe Created Successfully');
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    public function getAll()
+    {
+        try {
+            $recipes = Recipe::with('user')->get();
+    
+            return $this->customResponse($recipes);
+        } catch (Exception $e) {
+            return self::customResponse($e->getMessage(), 'error', 500);
+        }
+    }
+    
+    public function getById(Recipe $recipe){
+        try{
+            return $this->customResponse($recipe->load('user'));
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    public function destroy($id){
+        try{
+            $recipe = Recipe::find($id);
+
+            if (!$recipe) {
+                return $this->customResponse('recipe not found', 'error', 404);
+            }
+
+            $recipe->delete();
+            return $this->customResponse($recipe, 'Deleted Successfully');
+        }catch(Exception $e){
+            return self::customResponse($e->getMessage(),'error',500);
+        }
+    }
+
+    function customResponse($data, $status = 'success', $code = 200){
+        $response = ['status' => $status,'data' => $data];
+        return response()->json($response,$code);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RecipesController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+});
+
+
+Route::group(['middleware' => ['jwt.auth']], function () {
+
+    Route::get("/Home", [RecipesController::class, "getAll"]);
+
+    Route::group(['prefix' => 'recipe'], function(){
+        Route::get('/{recipe}', [RecipesController::class, "getById"]);
+        Route::post('/store', [RecipesController::class, "store"]);
+        Route::delete('/destroy/{id}', [RecipesController::class, "destroy"]);
+    });
+
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 });
